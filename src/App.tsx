e55d676bdf8ce631932d19bd8fdf579262a554d6
elazon <elazon01@gmail.com>
2024-07-07 05:26:05 -0500

    lint warnings

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,25 @@
-import './App.css'
-import Router from './router';
-import { useEffect } from 'react';
-import { company as co } from './docs';
+import "./App.css";
+import Router from "./router";
+import { useEffect } from "react";
+import { company as co } from "./docs";
 
-interface HTMLInputElementX extends Omit<HTMLInputElement, 'href'> {
-  href: string
+interface HTMLInputElementX extends Omit<HTMLInputElement, "href"> {
+  href: string;
 }
 
 function App() {
   useEffect(() => {
     const favicon_ = async () => {
-      let favicon = document.getElementById('favicon') as HTMLInputElementX;
-      if(!!co.favicon) favicon.href = co.favicon
-      else if(!!co.logo) favicon.href = co.logo
+      const favicon = document.getElementById("favicon") as HTMLInputElementX;
+      if (co.favicon) favicon.href = co.favicon;
+      else if (co.logo) favicon.href = co.logo;
     };
 
     favicon_();
     document.title = co.title;
   }, []);
 
-  return (
-      <Router />
-  )
+  return <Router />;
 }
 
-export default App
+export default App;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,52 +1,61 @@
-import sc from './../assets/sad_company.webp'
-import { Link } from 'react-router-dom'
-import ParticlesBg from './../components/ts_particles';
-import './../App.css'
-import { apis, company as co } from './../docs';
+import sc from "./../assets/sad_company.webp";
+import { Link } from "react-router-dom";
+import ParticlesBg from "./../components/ts_particles";
+import "./../App.css";
+import { apis, company as co } from "./../docs";
 
 function Home() {
-
   const mailTo = (email: string, type: string) => {
-    let subject = 'Need additional info about ' + (type == 'it' ? 'API' : 'product');
-    return 'mailto:' + email + '?Subject=' + subject;
-  }
+    const subject =
+      "Need additional info about " + (type == "it" ? "API" : "product");
+    return "mailto:" + email + "?Subject=" + subject;
+  };
 
-  const logo_ = (logo: string) => !!logo ? logo : sc
+  const logo_ = (logo: string) => (logo ? logo : sc);
 
   return (
     <>
       <ParticlesBg />
-      <div className='home'>
+      <div className="home">
         <div>
-          <a href={co.website} target='_blank'>
-            <img src={logo_(co.logo)} className='logo border-sc' alt='React logo' />
+          <a href={co.website} target="_blank">
+            <img
+              src={logo_(co.logo)}
+              className="logo border-sc"
+              alt="React logo"
+            />
           </a>
         </div>
         <h1>{co.title}</h1>
         <p>a documentation of {co.name} APis</p>
         <p>{co.about}</p>
-        <div className='docs-list'>
-          {apis && apis.map(({ name, page, id }) => (
-            <Link key={id} to={page}>
-              <div className='docs-card'>
-                <p className='white-text'>{name} ➡️</p>
-              </div>
-            </Link>
-          ))}
+        <div className="docs-list">
+          {apis &&
+            apis.map(({ name, page, id }) => (
+              <Link key={id} to={page}>
+                <div className="docs-card">
+                  <p className="white-text">{name} ➡️</p>
+                </div>
+              </Link>
+            ))}
         </div>
       </div>
-      <div className='social'>
+      <div className="social">
         <hr />
-        <p>Need help with API? <a href={mailTo(co['ti-mail'], 'it')}>Contact Support</a></p>
-        <p>Questions about product? <a href={mailTo(co['sales-mail'], 'sales')}>Contact Sales</a></p>
-      </div>
-      <div className='footer'>
-        <p className='white-text'>
-          Made with love 🩸
+        <p>
+          Need help with API?{" "}
+          <a href={mailTo(co["ti-mail"], "it")}>Contact Support</a>
+        </p>
+        <p>
+          Questions about product?{" "}
+          <a href={mailTo(co["sales-mail"], "sales")}>Contact Sales</a>
         </p>
       </div>
+      <div className="footer">
+        <p className="white-text">Made with love 🩸</p>
+      </div>
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
