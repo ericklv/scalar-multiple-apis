498e8a24749beb4b00957d449b84c552c3544961
elazon <elazon01@gmail.com>
2024-07-07 22:17:37 -0500

    add support for emitters in ts-particles

--- src/components/ts_particles.tsx
+++ src/components/ts_particles.tsx
@@ -1,16 +1,14 @@
 import { useEffect, useMemo } from "react";
 import Particles, { initParticlesEngine } from "@tsparticles/react";
 import { type Container } from "@tsparticles/engine";
-import { loadSlim } from "@tsparticles/slim";
+import { loadFull } from "tsparticles";;
 import { company as co } from '../config'
 import { amongus2 as config } from '../config/ts-particles'
 
 const ParticlesBg = () => {
     useEffect(() => {
         if (co["ts-particles"]) {
-            initParticlesEngine(async (engine) => {
-                await loadSlim(engine);
-            });
+            initParticlesEngine(async (engine) => { await loadFull(engine); });
         }
     }, []);
 
@@ -18,10 +16,7 @@ const ParticlesBg = () => {
         console.log(container);
     };
 
-    const options: any = useMemo(
-        () => (config),
-        [],
-    );
+    const options: any = useMemo(() => (config), [],);
 
     return (co["ts-particles"]) ?
         <Particles
