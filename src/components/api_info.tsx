062154bb408c47bf4d8071374836238a38ae53a9
elazon <elazon01@gmail.com>
2024-12-03 19:15:28 -0500

    update ts-particles

--- src/components/api_info.tsx
+++ src/components/api_info.tsx
@@ -1,8 +1,8 @@
 const APIInfo = ({ url }: { url: string | undefined }) => {
-    let url_: string = url ? url : "";
+    const url_: string = url ? url : "";
 
     const aux_type: string = url_.toUpperCase().split(".").pop()!;
-    let type = ["YAML", "YML", "JSON"].includes(aux_type) ? aux_type : "API";
+    const type = ["YAML", "YML", "JSON"].includes(aux_type) ? aux_type : "API";
 
     return <>
         <div className="api-type"><p>{type}</p></div>
